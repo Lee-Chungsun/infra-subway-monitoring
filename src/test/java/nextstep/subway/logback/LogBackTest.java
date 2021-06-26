925088145755564c48f9a1eea107f488edd55a91
Lee-Chungsun <cndtjs0218@naver.com>
2021-06-26 23:41:52 +0900

    feat : logback 필요 properties 추가

--- src/test/java/nextstep/subway/logback/LogBackTest.java
+++ src/test/java/nextstep/subway/logback/LogBackTest.java
@@ -1,2 +1,13 @@
-package nextstep.subway.logback;public class LogBackTest {
+package nextstep.subway.logback;
+
+import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("LogBack 테스트")
+public class LogBackTest {
+    @Test
+    void name() {
+        Station station = new Station("강남역");
+    }
 }
